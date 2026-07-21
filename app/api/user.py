3e4a59233e2a723936a14fdedb607f39e6d7e838
zyxweqx <nazyabarya228@gmail.com>
2026-07-21 14:49:01 +0200

    feat: add create and update operations for user

---
+++ app/api/user.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database import get_db
+from app.models.users import User
+from app.schemas.users import UserCreate
+
+router = APIRouter(prefix="/user", tags=["Users"])
+
+@router.post("/")
+async def create_user(user_in: UserCreate, db: AsyncSession = Depends(get_db)):
+    new_user = User(**user_in.model_dump())
+
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    return new_user
+
+@router.get("/{user_id}")
+async def get_user(user_id: int, db: AsyncSession = Depends(get_db)):
+    query = select(User).where(User.id == user_id)
+
+    result = await db.execute(query)
+
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.api.user import router as user_router
+from app.api.subscriptions import router as subscriptions_router
+app = FastAPI(
+    title="PolandRent Analytics"
+)
+
+app.include_router(user_router)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
